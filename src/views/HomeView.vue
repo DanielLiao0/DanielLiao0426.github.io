bbdc67564fd08ae50403815131ba722ea272c847
Daniel Liao <danielliao0426@gmail.com>
2023-12-02 02:24:17 -0700

    comment added in home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="app" class="min-h-screen flex flex-col relative">
+  <div id="app" class="min-h-screen flex flex-col relative">
     
     <div class="flex-1">
       <img
@@ -9,14 +9,19 @@
       />
     </div>
 
+    
     <div id="VehicleSelection" class="flex-1 relative">
+      
       <div class="w-full h-2 border-t"></div>
+
+      
       <div class="w-full text-center border-t-2 p-1.5 text-gray-700 text-lg font-semibold">
         
       </div>
 
+      <!-- Scrollable section for vehicle options -->
       <div class="scrollSection">
-
+        <!-- Vehicle option 1 -->
         <div class="bg-custom-gray">
           <div class="flex items-center px-4 py-5">
             <img width="75" src="img/robot/ride.png">
@@ -25,11 +30,11 @@
                 <div class="text-2xl mb-1">Max1</div>
                 
               </div>
-              
             </div>
           </div>
         </div>
 
+        <!-- Vehicle option 2 -->
         <div>
           <div class="flex items-center px-4 py-5">
             <img width="75" src="img/robot/ride.png">
@@ -38,11 +43,11 @@
                 <div class="text-2xl mb-1">Max2</div>
                 
               </div>
-              
             </div>
           </div>
         </div>
 
+        <!-- Vehicle option 3 -->
         <div>
           <div class="flex items-center px-4 py-5">
             <img width="75" src="img/robot/ride.png">
@@ -51,13 +56,13 @@
                 <div class="text-2xl mb-1">Max3</div>
                 
               </div>
-              
             </div>
           </div>
         </div>
 
       </div>
 
+      
       <div 
         class="
           flex 
@@ -79,30 +84,24 @@
   </div>
 </template>
 
-
-
 <style lang="scss">
-
   #map {
-    width:100%;
+    width: 100%;
     height: 10vh;
     top: 0px;
     left: 0px;
   }
 
-  .gm-style-cc {
-    display: none;
-  }
-
+  /* Styling for the VehicleSelection section */
   #VehicleSelection {
     .scrollSection {
       height: calc(50vh - 120px);
       position: relative;
       overflow-y: auto; 
       width: 100%;
-      
     }
 
+    /* Styling for the background color */
     .bg-custom-gray {
       background-color: rgb(237, 237, 237);
     }
